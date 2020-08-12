05da6e415280a5a81ce67fbba2f42617afa342ce
KalebLight <kaleb.luiz00@gmail.com>
2020-08-12 10:39:18 -0300

    Adicionando rotas e views ao projeto

--- Projeto - Portal de Notícias/noticias.js
+++ Projeto - Portal de Notícias/noticias.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express(); //executando a função contida em 'express'
 app.set('view engine', 'ejs')//.set => para modificar algo na tabela de propriedade do Express. Depois disso o Express entende que o gerador de views será o EJS.
-app.get('/',function(req, res){
-    res.send("<html><body>Portal de Notícias em Express</body></html>")
-})
 
-app.get('/tecnologia',function(req, res){
-    
-    res.render("secao/tecnologia")
+app.get('/',function(req, res){
+    res.render("home/index")
+})
+app.get('/formulario_inclusao_noticia',function(req, res){
+   res.render("admin/form_add_noticia")
+})
+app.get('/noticias', function(req, res){
+    res.render("noticias/noticias")
 })
 
 app.listen(3000, function(){
