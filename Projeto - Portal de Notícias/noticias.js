eaf16f99bd2b381af937b8a2c9dccc7d4b900ec1
KalebLight <kaleb.luiz00@gmail.com>
2020-08-12 10:31:03 -0300

    Adicionando EJS ao projeto e refatorando as views.

--- Projeto - Portal de Notícias/noticias.js
+++ Projeto - Portal de Notícias/noticias.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express(); //executando a função contida em 'express'
-
+app.set('view engine', 'ejs')//.set => para modificar algo na tabela de propriedade do Express. Depois disso o Express entende que o gerador de views será o EJS.
 app.get('/',function(req, res){
     res.send("<html><body>Portal de Notícias em Express</body></html>")
 })
 
 app.get('/tecnologia',function(req, res){
-    res.send("<html><body>Portal de Notícias (tecnológicas) em Express</body></html>")
+    
+    res.render("secao/tecnologia")
 })
 
 app.listen(3000, function(){
