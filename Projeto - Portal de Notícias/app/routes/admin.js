c9da45462b41219203ccbd9d6a7291cb855beacd
KalebLight <kaleb.luiz00@gmail.com>
2020-08-22 10:27:33 -0300

    Implementando inserção de noticias no banco de dados

---
+++ Projeto - Portal de Notícias/app/routes/admin.js
@@ -0,0 +1,21 @@
+const application = require("express")
+
+module.exports = function(application){ 
+    application.get('/formulario_inclusao_noticia',function(req, res){
+        res.render("admin/form_add_noticia")
+    })
+
+    application.post('/noticias/salvar', function(req, res){
+        let noticia = req.body        
+        let connection = application.config.dbConnection()
+        let noticiasModel = application.app.models.noticiasModel
+
+        noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+            res.redirect('/noticias')
+    }) 
+
+    })
+
+    
+    
+}
