b54571b5d63874b3937c98b5b927a0b1661d5a7b
KalebLight <kaleb.luiz00@gmail.com>
2020-08-31 21:47:00 -0300

    Atualizando recursos estáticos do projeto e refazendo lógica da Home

--- Projeto - Portal de Notícias/app/models/NoticiasDAO.js
+++ Projeto - Portal de Notícias/app/models/NoticiasDAO.js
@@ -14,6 +14,11 @@ NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
     this._connection.query('insert into noticias set ? ', noticia, callback)
 }
 
+NoticiasDAO.prototype.get5Ultimas = function(callback){
+    this._connection.query('select * from noticias order by data_craicao desc limit 5', callback)
+}
+    
+
 module.exports = function(){
  return NoticiasDAO;
 }
